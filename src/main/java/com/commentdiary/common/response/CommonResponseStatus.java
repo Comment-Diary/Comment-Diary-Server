cb0578378fe10bdc9d22e9411f28b93ff456da5b
Jeong-w-o-o <irmnels@gmail.com>
2022-03-26 23:28:45 +0900

    [FIX] 응답 메시지 수정

--- src/main/java/com/commentdiary/common/response/CommonResponseStatus.java
+++ src/main/java/com/commentdiary/common/response/CommonResponseStatus.java
@@ -5,14 +5,14 @@ import lombok.Getter;
 @Getter
 public enum CommonResponseStatus {
 
-    SUCCESS(1000, "요청에 성공하였습니다.")
+    SUCCESS("요청에 성공하였습니다.")
     ;
 
-    private final int code;
+//    private final int code;
     private final String message;
 
-    CommonResponseStatus(int code, String message) {
-        this.code = code;
+    CommonResponseStatus(String message) {
+//        this.code = code;
         this.message = message;
     }
 }
