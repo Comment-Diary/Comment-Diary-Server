bf0417686c85bcdd2f983ed188ee38ee29f49e16
Jeong-w-o-o <irmnels@gmail.com>
2022-03-28 00:44:40 +0900

    [FIX] 토큰 시간 임시 수정

--- src/main/java/com/commentdiary/jwt/TokenProvider.java
+++ src/main/java/com/commentdiary/jwt/TokenProvider.java
@@ -27,8 +27,8 @@ public class TokenProvider {
 
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60;
-    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 5;
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000L * 60 * 60 * 24 * 30;
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000L * 60 * 60 * 24 * 180;
 
     private final Key key;
 
