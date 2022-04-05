1ab2fbd4722f6675efb827eff9f05976f0fae61b
Jeong-w-o-o <irmnels@gmail.com>
2022-04-06 00:08:01 +0900

    [STYLE] 공백 줄 삭제

--- src/main/java/com/commentdiary/src/delivery/service/DeliveryService.java
+++ src/main/java/com/commentdiary/src/delivery/service/DeliveryService.java
@@ -26,7 +26,6 @@ import static com.commentdiary.common.exception.ErrorCode.NOT_FOUND_MEMBER;
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class DeliveryService {
-    private final DiaryRepository diaryRepository;
     private final MemberRepository memberRepository;
     private final DeliveryRepository deliveryRepository;
 
@@ -38,32 +37,6 @@ public class DeliveryService {
                 .filter(d -> d.getStatus().equals(DeliveryStatus.ACTIVE))
                 .orElseThrow(() -> new CommonException(NOT_FOUND_DELIVERY));
         return DeliveryResponse.of(delivery.getDiary(), member);
-    }
-
-    @Transactional
-    public void delivery() {
-        DeliveryRequest deliveryRequest = new DeliveryRequest();
-
-        Date today = new Date();
-        Date yesterday = new Date(today.getTime()+(1000*60*60*24*-1));
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd");
-
-        // 어제 일기를 찾아와서, 오늘 전달하는데, 전달 테이블에는 오늘 날짜를 저장해야 됨. 클라에서 오늘 날짜가 들어온다.
-        List<Member> members = memberRepository.findAll();
-        List<Diary> diaries = diaryRepository.findAllByDeliveryYnIsAndDateContainsAndMemberIsNotNullAndTempYnEquals('Y', simpleDateFormat.format(yesterday), 'N');
-
-        int idx = 0;
-
-        for (int i = 0; i < members.size(); i++) {
-            if (members.get(i).getId() == diaries.get(idx).getMember().getId()) {
-                idx = (idx + 1) % diaries.size();
-                if(diaries.size() == 1) {
-                    continue;
-                }
-            }
-            deliveryRepository.save(deliveryRequest.toEntity(members.get(i), diaries.get(idx), simpleDateFormat.format(today)));
-            idx = (idx + 1) % diaries.size();
-        }
     }
 
     private Long getMemberId() {
