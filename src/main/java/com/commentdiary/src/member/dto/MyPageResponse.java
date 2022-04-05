4d57e83a74b2fdfc3f7f5cc14aba19285f085a1d
Jeong-w-o-o <irmnels@gmail.com>
2022-04-05 22:55:58 +0900

    [FEAT] member: 마이페이지에서 푸시 알림 상태 조회 가능 추가

--- src/main/java/com/commentdiary/src/member/dto/MyPageResponse.java
+++ src/main/java/com/commentdiary/src/member/dto/MyPageResponse.java
@@ -11,15 +11,18 @@ import lombok.NoArgsConstructor;
 public class MyPageResponse {
     private String email;
     private double temperature;
+    private char pushYn;
 
-    public MyPageResponse(String email, double temperature) {
+    public MyPageResponse(String email, double temperature, char pushYn) {
         this.email = email;
+        this.pushYn = pushYn;
         this.temperature = temperature;
     }
 
     public static MyPageResponse of(Member member) {
         return MyPageResponse.builder()
                 .email(member.getEmail())
+                .pushYn(member.getPushYn())
                 .temperature(Math.round(member.getTemperature() * 10) / 10.0)
                 .build();
     }
