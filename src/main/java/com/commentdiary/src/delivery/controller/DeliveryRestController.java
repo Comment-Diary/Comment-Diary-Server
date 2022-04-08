7a6a17c200de64b066d11285feeb11e74f0a9d8a
Jeong-w-o-o <irmnels@gmail.com>
2022-04-08 19:18:43 +0900

    [FEAT] DeliveryRestController 로그 추가

--- src/main/java/com/commentdiary/src/delivery/controller/DeliveryRestController.java
+++ src/main/java/com/commentdiary/src/delivery/controller/DeliveryRestController.java
@@ -4,11 +4,14 @@ import com.commentdiary.common.response.CommonResponse;
 import com.commentdiary.src.delivery.dto.DeliveryResponse;
 import com.commentdiary.src.delivery.service.DeliveryService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/delivery")
@@ -19,8 +22,9 @@ public class DeliveryRestController {
      * 도착한 일기 조회
      */
     @GetMapping("")
-    public CommonResponse<DeliveryResponse> getDeliveredDiary(@RequestParam(name = "date") String date) {
+    public CommonResponse<DeliveryResponse> getDeliveredDiary(@RequestParam(name = "date") String date, Authentication authentication) {
         DeliveryResponse result = deliveryService.getDeliveredDiary(date);
+        log.info("[DeliveryRestController] getDeliveredDiary, id : {}", authentication.getName());
         return new CommonResponse<>(result);
     }
 }
