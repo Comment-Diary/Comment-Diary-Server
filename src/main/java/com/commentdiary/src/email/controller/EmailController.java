d15cf1f13b9130314380cd552701093970c392b6
Jeong-w-o-o <irmnels@gmail.com>
2022-04-08 19:19:56 +0900

    [FEAT] EamilController 로그 추가

--- src/main/java/com/commentdiary/src/email/controller/EmailController.java
+++ src/main/java/com/commentdiary/src/email/controller/EmailController.java
@@ -6,10 +6,12 @@ import com.commentdiary.src.email.dto.EmailAddrRequest;
 import com.commentdiary.src.email.dto.EmailSend;
 import com.commentdiary.src.email.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import static com.commentdiary.common.response.CommonResponseStatus.SUCCESS;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/email")
@@ -20,18 +22,21 @@ public class EmailController {
     @GetMapping("")
     public CommonResponse<Void> sendCode(@RequestParam(value = "email") EmailAddrRequest emailAddrRequest) {
         emailService.sendCode(new EmailSend(emailAddrRequest.getEmail(),"회원가입 인증 메일", ""));
+        log.info("[EmailController] sendCode");
         return new CommonResponse<>(SUCCESS);
     }
 
     @PostMapping("/confirm")
     public CommonResponse<Void> confirmCode(@RequestBody ConfirmCodeResquest confirmCodeResquest) {
         emailService.confirmCode(confirmCodeResquest);
+        log.info("[EmailController] confirmCode");
         return new CommonResponse<>(SUCCESS);
     }
 
     @GetMapping("/password")
     public CommonResponse<Void> sendPassword(@RequestParam(value = "email") EmailAddrRequest emailAddrRequest) {
         emailService.sendPassword(new EmailSend(emailAddrRequest.getEmail(),"임시 비밀번호 발급", ""));
+        log.info("[EmailController] sendPassword");
         return new CommonResponse<>(SUCCESS);
     }
 
