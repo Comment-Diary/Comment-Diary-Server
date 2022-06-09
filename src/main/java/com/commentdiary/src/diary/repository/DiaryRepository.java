5bdc88a31292dcc2a661b66f185354a52df19ea9
Jeong-w-o-o <irmnels@gmail.com>
2022-06-10 02:20:08 +0900

    [REFACTOR] 전달 일기 찾기 리팩토링

--- src/main/java/com/commentdiary/src/diary/repository/DiaryRepository.java
+++ src/main/java/com/commentdiary/src/diary/repository/DiaryRepository.java
@@ -2,17 +2,24 @@ package com.commentdiary.src.diary.repository;
 
 import com.commentdiary.src.diary.domain.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
     List<Diary> findByMemberIdOrderByDateDesc(long memberId);
+
     List<Diary> findByMemberIdAndDateContainsOrderByDateDesc(long memberId, String date);
 
-    Optional<Diary> findByIdAndMemberIdOrderByDateDesc(long diaryId, long memberId);
+    Optional<Diary> findByIdAndMemberId(long diaryId, long memberId);
 
     void deleteById(Long id);
 
-    List<Diary> findAllByDeliveryYnIsAndDateContainsAndMemberIsNotNullAndTempYnEquals(char deliveryYn, String date, char tempYn);
+    @Query("SELECT d " +
+            "FROM Diary d " +
+            "WHERE d.deliveryYn = 'Y' " +
+            "AND d.date = :date " +
+            "AND d.tempYn = 'N' AND d.member IS NOT NULL")
+    List<Diary> findAllByDeliveryDiaries(String date);
 }
