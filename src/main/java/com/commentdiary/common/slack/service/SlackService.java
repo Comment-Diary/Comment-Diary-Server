26a01621b09463be5a5de448ca081adebacc4bf9
Jeong-w-o-o <irmnels@gmail.com>
2022-06-19 18:19:33 +0900

    [FEAT] Slack 웹훅 서비스 추가

---
+++ src/main/java/com/commentdiary/common/slack/service/SlackService.java
@@ -0,0 +1,32 @@
+package com.commentdiary.common.slack.service;
+
+import com.slack.api.methods.SlackApiException;
+import org.springframework.beans.factory.annotation.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import com.slack.api.Slack;
+
+import java.io.IOException;
+
+@Slf4j
+@Service
+public class SlackService {
+    @Value("${slack.token}")
+    String token;
+
+    @Value("${slack.channel.id}")
+    String channel;
+
+    public void sendSlackMessage(String message) {
+        try {
+            Slack slack = Slack.getInstance();
+            slack.methods(token).chatPostMessage(req -> req
+                    .channel(channel)
+                    .text(message));
+        }
+        catch (SlackApiException | IOException e) {
+            log.error("Send Slack Message Error : {}", e.getMessage());
+        }
+
+    }
+}
