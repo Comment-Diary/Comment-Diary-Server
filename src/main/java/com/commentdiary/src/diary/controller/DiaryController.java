1db1fa2e1cbbf087b0ca1401ab15e070bf8add63
Jeong-w-o-o <irmnels@gmail.com>
2022-07-14 01:21:52 +0900

    [FEAT] 컨트롤러 진입 시 로그 추가

--- src/main/java/com/commentdiary/src/diary/controller/DiaryController.java
+++ src/main/java/com/commentdiary/src/diary/controller/DiaryController.java
@@ -8,6 +8,7 @@ import com.commentdiary.src.diary.dto.DiaryResponse;
 import com.commentdiary.src.diary.service.DiaryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,8 +27,9 @@ public class DiaryController {
      * 일기 작성
      */
     @PostMapping("")
-    public CommonResponse<CreateDiaryResponse> createDiary(@RequestBody CreateDiaryRequest createDiaryRequest) {
+    public CommonResponse<CreateDiaryResponse> createDiary(@RequestBody CreateDiaryRequest createDiaryRequest, Authentication authentication) {
         CreateDiaryResponse result = diaryService.createDiary(createDiaryRequest);
+        log.info("[DiaryController] createDiary, id : {}", authentication.getName());
         return new CommonResponse<>(result);
     }
 
@@ -35,8 +37,9 @@ public class DiaryController {
      * 일기 수정
      */
     @PatchMapping("/{diaryId}")
-    public CommonResponse<Void> updateDiary(@PathVariable(value = "diaryId") long diaryId, @RequestBody CreateDiaryRequest createDiaryRequest) {
+    public CommonResponse<Void> updateDiary(@PathVariable(value = "diaryId") long diaryId, @RequestBody CreateDiaryRequest createDiaryRequest, Authentication authentication) {
         diaryService.updateDiary(diaryId, createDiaryRequest);
+        log.info("[DiaryController] updateDiary, id : {}", authentication.getName());
         return new CommonResponse<>(SUCCESS);
     }
 
@@ -44,8 +47,9 @@ public class DiaryController {
      * 일기 삭제
      */
     @DeleteMapping("/{diaryId}")
-    public CommonResponse<Void> deleteDiary(@PathVariable(value = "diaryId") long diaryId) {
+    public CommonResponse<Void> deleteDiary(@PathVariable(value = "diaryId") long diaryId, Authentication authentication) {
         diaryService.deleteDiary(diaryId);
+        log.info("[DiaryController] deleteDiary, id : {}", authentication.getName());
         return new CommonResponse<>(SUCCESS);
     }
 
@@ -53,8 +57,9 @@ public class DiaryController {
      * 일기 메인 페이지 조회
      */
     @GetMapping("/main")
-    public CommonResponse<List<DiaryDetailResponse>> getAllMainDiary(@RequestParam(value = "date") String date) {
+    public CommonResponse<List<DiaryDetailResponse>> getAllMainDiary(@RequestParam(value = "date") String date, Authentication authentication) {
         List<DiaryDetailResponse> result = diaryService.getAllMainDiary(date);
+        log.info("[DiaryController] getAllMainDiary, id : {}", authentication.getName());
         return new CommonResponse<>(result);
     }
 
@@ -63,8 +68,9 @@ public class DiaryController {
      * 특정 일자의 일기 id, 제목, 내용, 코멘트 리턴
      */
     @GetMapping("/{diaryId}")
-    public CommonResponse<DiaryDetailResponse> getOneDiary(@PathVariable(value = "diaryId") long diaryId) {
+    public CommonResponse<DiaryDetailResponse> getOneDiary(@PathVariable(value = "diaryId") long diaryId, Authentication authentication) {
         DiaryDetailResponse result = diaryService.getOneDiary(diaryId);
+        log.info("[DiaryController] getOneDiary, id : {}", authentication.getName());
         return new CommonResponse<>(result);
     }
 
@@ -72,7 +78,8 @@ public class DiaryController {
      * 일기 모아보기 (검색 필터: 전체)
      */
     @GetMapping("/my/all")
-    public CommonResponse<List<DiaryResponse>> getDiaryByAllDate() {
+    public CommonResponse<List<DiaryResponse>> getDiaryByAllDate(Authentication authentication) {
+        log.info("[DiaryController] getDiaryByAllDate, id : {}", authentication.getName());
         return new CommonResponse<>(diaryService.getDiaryByAllDate());
     }
 
@@ -80,9 +87,9 @@ public class DiaryController {
      * 일기 모아보기 (검색 필터: 특정 달)
      */
     @GetMapping("/my")
-    public CommonResponse<List<DiaryResponse>> getDiaryByDate(@RequestParam(value = "date") String date) {
+    public CommonResponse<List<DiaryResponse>> getDiaryByDate(@RequestParam(value = "date") String date, Authentication authentication) {
+        log.info("[DiaryController] getDiaryByDate, id : {}", authentication.getName());
         return new CommonResponse<>(diaryService.getDiaryByDate(date));
     }
 
 }
-
--- src/main/java/com/commentdiary/src/member/controller/MemberController.java
+++ src/main/java/com/commentdiary/src/member/controller/MemberController.java
@@ -4,6 +4,8 @@ import com.commentdiary.common.response.CommonResponse;
 import com.commentdiary.src.member.dto.*;
 import com.commentdiary.src.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
@@ -17,7 +19,7 @@ import java.util.Map;
 import static com.commentdiary.common.exception.ErrorCode.INVALID_EMAIL_ADDRESS;
 import static com.commentdiary.common.response.CommonResponseStatus.SUCCESS;
 
-@Validated
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/members")
@@ -26,14 +28,8 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/sign-up")
-    public CommonResponse<Void> signUp(@Valid @RequestBody SignUpRequest signUpRequest, BindingResult result) {
-        if(result.hasErrors()) {
-            Map<String, String> errorMap = new HashMap<>();
-            for(FieldError error : result.getFieldErrors()) {
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-            return new CommonResponse(INVALID_EMAIL_ADDRESS);
-        }
+    public CommonResponse<Void> signUp(@Valid @RequestBody SignUpRequest signUpRequest) {
+        log.info("[MemberController] signUp");
         memberService.signUp(signUpRequest);
         return new CommonResponse<>(SUCCESS);
     }
@@ -41,54 +37,63 @@ public class MemberController {
     @PostMapping("/login")
     public CommonResponse<TokenResponse> login(@Valid @RequestBody LoginRequest loginRequest) {
         TokenResponse result = memberService.login(loginRequest);
+        log.info("[MemberController] login");
         return new CommonResponse<>(result);
     }
 
     @PostMapping("/auth-login")
     public CommonResponse<AuthLoginResponse> authLogin(@RequestBody AuthLoginRequest authLoginRequest) {
         AuthLoginResponse result = memberService.authLogin(authLoginRequest);
+        log.info("[MemberController] authLogin");
         return new CommonResponse<>(result);
     }
 
     @PostMapping("/oauth-sign-up")
-    public CommonResponse<Void> oauthSignUp(@RequestBody OAuthSignUpRequest oAuthSignUpRequest) {
+    public CommonResponse<Void> oauthSignUp(@RequestBody OAuthSignUpRequest oAuthSignUpRequest, Authentication authentication) {
         memberService.oauthSignUp(oAuthSignUpRequest);
+        log.info("[MemberController] oauthSignUp, id : {}", authentication.getName());
         return new CommonResponse<>(SUCCESS);
     }
 
     @GetMapping("")
-    public CommonResponse<MyPageResponse> myPage() {
+    public CommonResponse<MyPageResponse> myPage(Authentication authentication) {
         MyPageResponse result = memberService.myPage();
+        log.info("[MemberController] myPage, id : {}", authentication.getName());
         return new CommonResponse<>(result);
     }
 
     @PatchMapping("")
-    public CommonResponse<Void> changePassword(@RequestBody ChangePasswordRequest changePasswordRequest) {
+    public CommonResponse<Void> changePassword(@RequestBody ChangePasswordRequest changePasswordRequest, Authentication authentication) {
         memberService.changePassword(changePasswordRequest);
+        log.info("[MemberController] changePassword, id : {}", authentication.getName());
         return new CommonResponse<>(SUCCESS);
     }
 
     @DeleteMapping("/logout")
     public CommonResponse<Void> logout(@RequestHeader(value = "X-AUTH-TOKEN") String accessToken) {
         memberService.logout(accessToken);
+        log.info("[MemberController] logout");
         return new CommonResponse<>(SUCCESS);
     }
 
     @PatchMapping("/push")
-    public CommonResponse<PushDto> push() {
+    public CommonResponse<PushDto> push(Authentication authentication) {
         PushDto result = memberService.push();
+        log.info("[MemberController] push, id : {}", authentication.getName());
         return new CommonResponse<>(result);
     }
 
     @DeleteMapping("")
-    public CommonResponse<Void> delete() {
+    public CommonResponse<Void> delete(Authentication authentication) {
         memberService.delete();
+        log.info("[MemberController] delete, id : {}", authentication.getName());
         return new CommonResponse<>(SUCCESS);
     }
 
     @PostMapping("/reissue")
     public CommonResponse<TokenResponse> reissue(@RequestHeader(value = "X-AUTH-TOKEN") String accessToken, @RequestHeader(value = "REFRESH-TOKEN") String refreshToken) {
         TokenResponse reuslt = memberService.reissue(accessToken, refreshToken);
+        log.info("[MemberController] reissue");
         return new CommonResponse<>(reuslt);
     }
 }
