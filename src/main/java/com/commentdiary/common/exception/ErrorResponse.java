0d407025a9cf09abcc17adcbd36525d0fad93190
Jeong-w-o-o <irmnels@gmail.com>
2022-07-14 02:10:07 +0900

    [FEAT] Valid Exception 추가

--- src/main/java/com/commentdiary/common/exception/ErrorResponse.java
+++ src/main/java/com/commentdiary/common/exception/ErrorResponse.java
@@ -5,13 +5,15 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Map;
+
 @Getter
 @Setter
 @Builder
-public class ErrorResponse {
+public class ErrorResponse<T> {
     private int status;
     private String code;
-    private String message;
+    private T message;
 
     public static ResponseEntity<ErrorResponse> toCommonExceptionEntity(ErrorCode errorCode) {
         return ResponseEntity
@@ -20,6 +22,17 @@ public class ErrorResponse {
                         .status(errorCode.getHttpStatus().value())
                         .code(errorCode.name())
                         .message(errorCode.getMessage())
+                        .build());
+    }
+
+    public static ResponseEntity<ErrorResponse> toValidationException(Map<String, String> errors) {
+        ErrorCode errorCode = ErrorCode.INVALID_INPUT_VALUE;
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(ErrorResponse.builder()
+                        .status(errorCode.getHttpStatus().value())
+                        .code(errorCode.name())
+                        .message(errors)
                         .build());
     }
 
--- src/main/java/com/commentdiary/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/commentdiary/common/exception/GlobalExceptionHandler.java
@@ -3,8 +3,15 @@ package com.commentdiary.common.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
@@ -16,9 +23,21 @@ public class GlobalExceptionHandler {
         return ErrorResponse.toCommonExceptionEntity(e.getErrorCode());
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> validationException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+
+        Map<String, String> errors = new HashMap<>();
+        bindingResult.getAllErrors()
+                .forEach(c -> errors.put(((FieldError) c).getField(), c.getDefaultMessage()));
+
+        return ErrorResponse.toValidationException(errors);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> allHandleException(Exception e){
         log.error("Handle All Exception: {}", e.getMessage());
         return ErrorResponse.toAllExceptionEntity(e.getMessage());
     }
+
 }
